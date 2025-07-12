090f40e1e0b93ca7ec36c86b2715e0bf45560044
Axel Aguado <axelaguado17@gmail.com>
2025-07-12 14:53:37 -0300

    Implementamos el metodo POST para producto

--- clases/producto.class.php
+++ clases/producto.class.php
@@ -3,7 +3,20 @@ require_once "conexion/conexion.php";
 require_once "clases/respuesta.php";
 
 class producto extends conexion {
+
+    // Estamos modelando la tabla?.
     private $table = "producto";
+
+    // Definimos los campos de la tabla. 
+    private $productoId;
+    private $marca = "";
+    private $modelo = "";
+    private $categoria;
+    private $precio = 0;
+    private $stock  = 0;
+    private $descripcion = "";
+    private $imagen = "";
+    private $estado = 0; 
 
     public function listarProductos($pagina = 1) {
         $inicio = 0; // fila en la que arranca.
@@ -33,5 +46,81 @@ class producto extends conexion {
         return $datos;
     }
 
+    // Implementamos metodo para validar los datos.
+    public function post($json) 
+    {
+        $respuesta = new respuesta;
+        
+        //Como lo que recibimos es un json, primero lo decodificamos como array asociativo. 
+        $datos = json_decode($json, true);
+
+        if(!isset($datos['productoId']) || !isset($datos['categoria'])) 
+        {
+            return $respuesta->error_400();
+        } 
+        else
+        {
+            //Falta manejar errores de las variable no asignadas.
+            $this->productoId = $datos['productoId'];
+
+            if(isset($datos['marca'])){
+                $this->marca =  $datos['marca'];
+            }
+            
+            if(isset($datos['modelo'])){
+                $this->modelo =  $datos['modelo'];
+            }
+             
+            $this->categoria =  $datos['categoria'];
+
+            if(isset($datos['precio'])){
+               $this->precio =  $datos['precio'];
+            }
+             
+            if(isset($datos['stock'] )){
+                $this->stock = $datos['stock']  ;
+            }
+
+            if(isset( $datos['descripcion'])){
+                $this->descripcion = $datos['descripcion'] ;
+            } 
+
+            // Ver que onda el tratamiento de imagenes.
+            if(isset($datos['imagen'] )){
+                $this->imagen =  $datos['imagen'];
+            }
+
+            if(isset($datos['estado'] )){
+                 $this->estado =  $datos['estado'];  
+            } 
+
+            $response = $this->insertarProducto(); 
+
+            if ($response == -1) {
+                return  $respuesta->error_500(); 
+            } else {
+                $result = $respuesta->response;
+                $result['result'] = array(
+                    'Producto' => 'El producto se registro correctamente [' . $response . ']' 
+                );
+                return $result;
+            }
+        } 
+    }
+
+
+    // Implementamos metodo que gestione la insercion.
+    public function insertarProducto()
+    {
+        // Ya hemos verificado que la query se forme como corresponde.
+        $query = "INSERT INTO " . $this->table . " (productoId, marca, modelo, categoria, precio, stock,
+        descripcion, imagen, estado) VALUES (" . $this->productoId . ", '" . $this->marca . "', 
+        '" . $this->modelo . "', " . $this->categoria . ", " . $this->precio . ", " . $this->stock . ", '"
+        . $this->descripcion . "', '" . $this->imagen . "', " . $this->estado . ")"; 
+
+        $verificar = parent::nonQuery($query); 
+ 
+        return $verificar;   
+    } 
 }
 ?>
